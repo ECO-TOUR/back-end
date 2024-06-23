05b63c1e493a3e01cf1b306b6dfbe8683acc3cd3
jinho4124 <jh.yoon@sparta-devcamp.com>
2024-06-23 15:10:17 +0900

    fix: 회원가입 폼 커스텀 유저 모델로 변경 반영

--- ecotour/accounts/admin.py
+++ ecotour/accounts/admin.py
@@ -10,7 +10,7 @@ from .models import CustomUser
 @admin.register(RefreshTokenModel)
 class RefreshTokenModelAdmin(admin.ModelAdmin):
     list_display = ("user", "token", "jti", "created_at", "expires_at", "blacklisted")
-    search_fields = ("user__u   sername", "token", "jti")
+    search_fields = ("user__username", "token", "jti")
     list_filter = ("blacklisted", "created_at", "expires_at")
 
 
