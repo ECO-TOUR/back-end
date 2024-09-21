00a6d0bfa873c0faee1f40b65c65b612b86e0382
novzs <novzs@naver.com>
2024-09-22 01:21:29 +0900

    fix : config 파일 cors 주소 추가

--- ecotour/config/settings.py
+++ ecotour/config/settings.py
@@ -193,6 +193,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://localhost:3000",
     "https://127.0.0.1:3000",
+    "https://ecotour.seedoseedo.site",
     # Add more origins as needed
 ]
 
